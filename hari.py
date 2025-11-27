cde016c4053dd0499ab049c8ec1f23e23d85f4a3
Krishna Dhanuk-KD <dhanukkrishna0987@gmail.com>
2025-11-27 15:50:33 +0545

    Added new files

---
+++ hari.py
@@ -0,0 +1,11 @@
+print("Envrio module loaded")
+
+import numpy as np
+a = np.array([1, 2, 3])
+print("Numpy array created:", a)
+
+import matplotlib.pyplot as plt
+a = np.array([1, 2, 3, 4, 5])
+plt.plot(a)
+plt.title("Simple Plot")
+plt.show()
